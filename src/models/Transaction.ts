7de03470ef2afc83f7ec5c026348d0d8c4a6fbff
VITOR PEREIRA <vop1234@hotmail.com>
2020-04-30 17:25:01 -0300

    Import

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -5,7 +5,7 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   ManyToOne,
-  JoinColumn
+  JoinColumn,
 } from 'typeorm'
 
 import Category from "./Category";
@@ -24,9 +24,9 @@ class Transaction {
   @Column('decimal')
   value: number;
 
-  @ManyToOne(() => Category)
+  @ManyToOne(() => Category, category => category.transaction, { eager: true })
   @JoinColumn({ name: 'category_id' })
-  category: Category
+  category: Category;
 
   @Column()
   category_id: string;
